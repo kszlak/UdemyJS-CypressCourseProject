5545df3becf856a40dc2e6e95dfd893139d867c9
kornelia.szlak <kornelia.szlak@adverity.com>
2023-08-16 12:20:31 +0200

    Initial commit

---
+++ basics2.js
@@ -0,0 +1,9 @@
+const flag = true
+ if(!flag)
+ {
+     console.log("condision ok")
+ }
+ else
+ {
+    console.log("condition not ok")
+ }
---
+++ hello.js
@@ -0,0 +1,30 @@
+console.log("hello")
+let a = 4
+console.log(a)
+console.log(typeof(a))
+
+let b = 4.12
+console.log(typeof(b))
+
+let c = "Nela"
+console.log(typeof(c))
+
+let d = true
+console.log(typeof(d))
+console.log(!d)
+//!
+
+//var,let,const
+//null and undefined
+/* comments
+more and more
+ */
+let sumaab = a+b
+console.log(sumaab)
+//we cannot redeclare with let variable but we can do with var
+//reassigning works with let
+c = a=b //reassigning, let c =a=b redeclaring
+
+const x=a+b // can't be reassign e.g. x=a-b
+
+

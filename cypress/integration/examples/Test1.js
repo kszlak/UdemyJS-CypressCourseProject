952258cffd35b9fc851f0a74bf56d08181b5c2d6
kornelia.szlak <kornelia.szlak@adverity.com>
2023-08-21 22:26:25 +0200

    add a few new assertion examples to the first test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -13,5 +13,32 @@ describe('My First e2e Test Suite', function()
         cy.get('.search-keyword').type('ca') //get access/find element
         cy.wait(2000)
         cy.get('.product:visible').should('have.length',4) //visible added to get only visible items
+
+        //Parent Child chaining
+        cy.get('.products').as('productLocator') //aliasing to use locators
+        cy.get('@productLocator').find('.product').should('have.length',4) //the same result as above, checked only in products block
+        cy.get(':nth-child(3) > .product-action > button').click()
+        console.log('test') //print in the developer tools in the console, it's js
+        //added with index
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click() //eq: to get the third one (indexing from 0)
+
+        //added dynamically with using each and iterate of each element of the array
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click()
+            }
+        })
+
+        //assert if logo is correctly displayed
+        cy.get('.brand').should('have.text',('GREENKART'))
+
+        //this is to print in logs
+        cy.get('.brand').then(function(logoelement) //manually resolving the promise with then()
+        {
+            cy.log(logoelement.text()) //log: to print output / text() is a JQuery not Cypress method
+        })
     })
 })
+
+

c519327e1a5e844f4940938bc50e66f7145035f2
kornelia.szlak <kornelia.szlak@adverity.com>
2023-08-22 23:00:16 +0200

    add third test with dropdowns and buttons examples

---
+++ cypress/integration/examples/Test3.js
@@ -0,0 +1,40 @@
+describe('My Third e2e Test Suite', function()
+{
+    it('My Third Test Case', function()
+    {
+        //checkboxes
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check(['option2', 'option3']) //get all checkboxes and check 2 of them
+
+        //static dropdowns
+        cy.get('select').select('option2').should('have.value', 'option2')
+
+        //dynamic dropdowns
+        cy.get('#autocomplete').type('ind')
+        cy.get('.ui-menu-item div').each(($el, index, $list) =>
+        {
+            if($el.text()==='India')
+            {
+                cy.wrap($el).click()
+            }
+
+        })
+
+        //autocomplete
+        cy.get('#autocomplete').should('have.value', 'India')
+
+        //visible/invisible elements
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('not.be.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+
+        //radio buttons
+        cy.get('[value="radio2"]').check().should('be.checked')
+
+        })
+
+})

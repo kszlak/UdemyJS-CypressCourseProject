98016546ecc1c4b1f60c9b635f08887d4803eaf3
kornelia.szlak <kornelia.szlak@adverity.com>
2023-08-28 15:08:50 +0200

    add new examples using example.json and commands.js files

---
+++ cypress/integration/examples/Test9_Framework.js
@@ -0,0 +1,42 @@
+describe('Frame test', function()
+{
+    //runs once before all tests in the block
+    before(function (){
+
+        //to call the file from fixture folder
+        cy.fixture('example').then(function(data)
+        {
+            //this will add data access to entire class, makes it global
+            this.data=data
+        })
+    })
+
+    it('Validate elements in the iframe', function()
+    {
+        cy.visit('https://www.rahulshettyacademy.com/angularpractice/')
+        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        cy.get('select').select(this.data.gender)
+        cy.get('input[name="name"]:nth-child(1)').should('have.value', this.data.name)
+
+        //to validate any attribute value
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', 2)
+
+        //to validate if radio button is disabled
+        cy.get('#inlineRadio3').should('be.disabled')
+
+        // use cy.pause() or debug() to debug
+        // e.g. cy.pause()
+        // e.g. cy.get(':nth-child(2) > .nav-link').click().debug()
+
+        cy.get(':nth-child(2) > .nav-link').click()
+
+        //call the function from commands.js with productName parameter
+        //we don't need to import any file as it is in default file commands.js in support folder
+        //data come from example.json
+        this.data.productName.forEach(function(element)
+        {
+            cy.selectProduct(element)
+        })
+
+    })
+})

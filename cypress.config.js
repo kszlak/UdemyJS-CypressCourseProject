34faec3a3a191c32484f154e5fe7fe7635b88030
kornelia.szlak <kornelia.szlak@adverity.com>
2023-08-30 17:55:02 +0200

    increase timeout for all test cases

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000, //timeout for all test cases
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here

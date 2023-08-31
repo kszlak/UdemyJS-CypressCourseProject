5b7555767e04f6bb818725c74758df75ab63cfe1
kornelia.szlak <kornelia.szlak@adverity.com>
2023-08-31 17:12:49 +0200

    add global variables - env and retries

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 6000, //timeout for all test cases
+  env: {
+    url: 'https://rahulshettyacademy.com'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern:'cypress/integration/examples/*.js'
+  },
+  projectId: "vmp44e",
+  retries: {
+    runMode: 1, //change e.g. to 1 when you want your test run again when fail
   },
 });
